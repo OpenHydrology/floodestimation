875e95e386aa2bb08432b4e02cc43aa83d67cdd7
faph <faph@users.noreply.github.com>
2014-08-27 00:41:44 +0100

    QMED from 1999 descriptors

--- floodestimation/catchment.py
+++ floodestimation/catchment.py
@@ -7,15 +7,29 @@ from math import log
 class Catchment(object):
     """
     Catchment object include FEH catchment descriptors and additional information describing the catchment.
+
+    Example:
+
+    >>> from floodestimation.catchment import Catchment
+    >>> catchment = Catchment("Aberdeen", "River Dee")
+    >>> catchment.channel_width = 1
+    >>> catchment.descriptors = {'area': 1, 'bfihost': 0.50, 'sprhost': 50, 'saar': 1000, 'farl': 1}
+    >>> catchment.qmed_all()
+    {'area': 1.172, 'descriptors_1999': 0.2671386414098229, 'channel_width': 0.182}
+
+
     """
 
+    #: Methods available to estimate QMED
+    qmed_methods = ('channel_width', 'area', 'descriptors_1999')
+
     def __init__(self, location, watercourse=None):
-        # : Catchment outlet location name, e.g. `Aberdeen`
+        #: Catchment outlet location name, e.g. `Aberdeen`
         self.location = location
         self._watercourse = watercourse
-        # : FEH catchment descriptors as a dict
+        #: FEH catchment descriptors as a dict
         self.descriptors = {}
-        # : Width of the watercourse channel at the catchment outlet in m.
+        #: Width of the watercourse channel at the catchment outlet in m.
         self.channel_width = None
 
     @property
@@ -31,6 +45,15 @@ class Catchment(object):
     @watercourse.setter
     def watercourse(self, value):
         self._watercourse = value
+
+    def qmed_all(self):
+        result = {}
+        for method in self.qmed_methods:
+            try:
+                result[method] = getattr(self, 'qmed_from_' + method)()
+            except:
+                result[method] = None
+        return result
 
     def _ae(self):
         return 1 - 0.015 * log(self.descriptors['area'] / 0.5)  # this is ln(2*A)
@@ -59,17 +82,23 @@ class Catchment(object):
         :type: float
         """
         try:
-            return 1.172 * self.descriptors['area'] ** self._ae()
-        except TypeError:
+            return 1.172 * self.descriptors['area'] ** self._ae()  # Area in km²
+        except (TypeError, KeyError):
             raise Exception("Catchment `descriptors` attribute must be set first.")
 
     def qmed_from_descriptors_1999(self):
-        reshost = self.descriptors['bfihost'] + 1.3 * (self.descriptors['sprhost'] / 100.0) - 0.987
+        """
+        Return QMED estimation based on FEH catchment descriptors, 1999 methodology.
+
+        :return: QMED in m³/s
+        :type: float
+        """
         try:
+            reshost = self.descriptors['bfihost'] + 1.3 * (self.descriptors['sprhost'] / 100.0) - 0.987
             return 1.172 * self.descriptors['area'] ** self._ae() \
                    * (self.descriptors['saar'] / 1000.0) ** 1.560 \
                    * self.descriptors['farl'] ** 2.642 \
                    * (self.descriptors['sprhost'] / 100.0) ** 1.211 * \
                    0.0198 ** reshost
-        except TypeError:
+        except (TypeError, KeyError):
             raise Exception("Catchment `descriptors` attribute must be set first.")
--- floodestimation/tests/test_catchment_qmed.py
+++ floodestimation/tests/test_catchment_qmed.py
@@ -20,8 +20,10 @@ class TestCatchmentQmed(unittest.TestCase):
 
     def test_channel_no_width(self):
         catchment = Catchment("Aberdeen", "River Dee")
-        with self.assertRaises(Exception):
-            qmed = catchment.qmed_from_channel_width()
+        try:
+            catchment.qmed_from_channel_width()
+        except Exception as e:
+            self.assertEqual(str(e), "Catchment `channel_width` attribute must be set first.")
 
     def test_area_1(self):
         catchment = Catchment("Aberdeen", "River Dee")
@@ -40,5 +42,54 @@ class TestCatchmentQmed(unittest.TestCase):
 
     def test_no_area(self):
         catchment = Catchment("Aberdeen", "River Dee")
-        with self.assertRaises(Exception):
-            qmed = catchment.qmed_from_area()
+        try:
+            catchment.qmed_from_area()
+        except Exception as e:
+            self.assertEqual(str(e), "Catchment `descriptors` attribute must be set first.")
+
+    def test_descriptors_1999_1(self):
+        catchment = Catchment("Aberdeen", "River Dee")
+        catchment.descriptors = {'area': 1,
+                                 'bfihost': 0.50,
+                                 'sprhost': 50,
+                                 'saar': 1000,
+                                 'farl': 1}
+        self.assertEqual(round(catchment.qmed_from_descriptors_1999(), 4), 0.2671)
+
+    def test_descriptors_1999_2(self):
+        catchment = Catchment("Aberdeen", "River Dee")
+        catchment.descriptors = {'area': 2.345,
+                                 'bfihost': 0,
+                                 'sprhost': 100,
+                                 'saar': 2000,
+                                 'farl': 0.5}
+        self.assertEqual(round(catchment.qmed_from_descriptors_1999(), 4), 0.3729)
+
+    def test_descriptors_1999_3(self):
+        catchment = Catchment("Aberdeen", "River Dee")
+        catchment.descriptors = {'area': 100,
+                                 'bfihost': 0.50,
+                                 'sprhost': 50,
+                                 'saar': 1000,
+                                 'farl': 1}
+        self.assertEqual(round(catchment.qmed_from_descriptors_1999(), 4), 18.5262)
+
+    def test_no_descriptors_1999(self):
+        catchment = Catchment("Aberdeen", "River Dee")
+        try:
+            catchment.qmed_from_descriptors_1999()
+        except Exception as e:
+            self.assertEqual(str(e), "Catchment `descriptors` attribute must be set first.")
+
+    def test_all(self):
+        catchment = Catchment("Aberdeen", "River Dee")
+        catchment.channel_width = 1
+        catchment.descriptors = {'area': 1,
+                                 'bfihost': 0.50,
+                                 'sprhost': 50,
+                                 'saar': 1000,
+                                 'farl': 1}
+        qmeds = catchment.qmed_all()
+        self.assertEqual(qmeds['channel_width'], 0.182)
+        self.assertEqual(qmeds['area'], 1.172)
+        self.assertEqual(round(qmeds['descriptors_1999'], 4), 0.2671)
