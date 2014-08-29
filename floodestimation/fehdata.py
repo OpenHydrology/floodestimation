e0aa7f94a2de1576b67dc7806e4a700df0f7d925
faph <faph@users.noreply.github.com>
2014-08-29 23:36:10 +0100

    Initial setup of fehdata incl. download

--- floodestimation/fehdata.py
+++ floodestimation/fehdata.py
@@ -1,6 +1,7 @@
 from urllib.request import urlopen
 from appdirs import AppDirs
 import os
+import shutil
 from zipfile import ZipFile
 
 DOWNLOAD_URL = 'http://www.ceh.ac.uk/data/nrfa/peak_flow/WINFAP-FEH_v3.3.4.zip'
@@ -23,6 +24,19 @@ def unzip_data():
     with ZipFile(os.path.join(CACHE_FOLDER, CACHE_ZIP), 'r') as zf:
         zf.extractall(path=CACHE_FOLDER)
 
+
+def clear_cache():
+    shutil.rmtree(CACHE_FOLDER)
+    os.makedirs(CACHE_FOLDER)
+
+
+def amax_files():
+    return [os.path.join(dp, f) for dp, dn, filenames in os.walk(CACHE_FOLDER)
+            for f in filenames if os.path.splitext(f)[1].lower() == '.am']
+
+def cd3_files():
+    return [os.path.join(dp, f) for dp, dn, filenames in os.walk(CACHE_FOLDER)
+            for f in filenames if os.path.splitext(f)[1].lower() == '.am']
 
 def update_database():
     """
--- floodestimation/tests/test_fehdata.py
+++ floodestimation/tests/test_fehdata.py
@@ -4,9 +4,25 @@ import os
 
 
 class TestDatabase(unittest.TestCase):
-    def test_download(self):
+    @unittest.skip
+    def test_a_clear_cache(self):
+        fehdata.clear_cache()
+        self.assertFalse(os.listdir(fehdata.CACHE_FOLDER))
+
+    @unittest.skip
+    def test_b_download(self):
         fehdata.download_data()
         self.assertTrue(os.path.isfile(os.path.join(fehdata.CACHE_FOLDER, fehdata.CACHE_ZIP)))
 
-    def test_unzip(self):
+    @unittest.skip
+    def test_c_unzip(self):
         fehdata.unzip_data()
+        self.assertTrue(os.path.isdir(os.path.join(fehdata.CACHE_FOLDER, 'Not suitable for QMED or Pooling')))
+        self.assertTrue(os.path.isdir(os.path.join(fehdata.CACHE_FOLDER, 'Suitable for Pooling')))
+        self.assertTrue(os.path.isdir(os.path.join(fehdata.CACHE_FOLDER, 'Suitable for QMED')))
+
+    def test_d_amax_files(self):
+        self.assertTrue(len(fehdata.amax_files()), 963)
+
+    def test_e_cd3_files(self):
+        self.assertTrue(len(fehdata.cd3_files()), 963)
