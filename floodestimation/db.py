0bc262d0e6aa333802affdd17d9b96c850dc208b
faph <faph@users.noreply.github.com>
2014-09-08 21:14:16 +0100

    Code tidying and licence info

--- floodestimation/db.py
+++ floodestimation/db.py
@@ -1,10 +1,23 @@
+# Copyright (c) 2014  Florenz A.P. Hollebrandse <f.a.p.hollebrandse@protonmail.ch>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 import floodestimation.settings as settings
-
-
 
 # Base class all entities that should be stored as a table in the database should be inheriting from
 Base = declarative_base()
