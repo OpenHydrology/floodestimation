101592bf7f7f5291556420a920320ce981286f1b
faph <faph@users.noreply.github.com>
2014-09-09 20:21:40 +0100

    Removed FileNotFoundError not supported by python 3.2

--- floodestimation/tests/test_db.py
+++ floodestimation/tests/test_db.py
@@ -1,5 +1,6 @@
 import unittest
 import os
+from errno import ENOENT
 
 import floodestimation.settings as settings
 from floodestimation.db import Base, engine
@@ -9,14 +10,16 @@ class TestDatabaseCreation(unittest.TestCase):
     def setUp(self):
         try:
             os.remove(settings.DB_FILE_PATH)
-        except FileNotFoundError:
-            pass
+        except OSError as e:
+            if not e.errno == ENOENT:  # File does not exist
+                raise
 
     def tearDown(self):
         try:
             os.remove(settings.DB_FILE_PATH)
-        except FileNotFoundError:
-            pass
+        except OSError as e:
+            if not e.errno == ENOENT:  # File does not exist
+                raise
 
     def test_create_new_db_on_import(self):
         self.assertEqual(['amaxrecords', 'catchments', 'comments', 'descriptors'],
