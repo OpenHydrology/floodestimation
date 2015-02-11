7edd07f967c390d4b248261cd377e7835671106b
Florenz A. P. Hollebrandse <faph@users.noreply.github.com>
2015-02-11 17:45:05 +0000

    setup tidy

---
+++ README.md
@@ -0,0 +1,10 @@
+# floodestimation
+
+Python library for flood frequency-magnitude analyses for UK catchments. Part of the
+[Open Hydrology](http://open-hydrology.org) initiative.
+
+Documentation at: <http://docs.open-hydrology.org/projects/floodestimation>.
+
+Source code at: <https://github.com/OpenHydrology/floodestimation>.
+
+Licensed under the [GPLv3 licence](http://www.gnu.org/copyleft/gpl.html).
--- setup.py
+++ setup.py
@@ -1,21 +1,10 @@
 from setuptools import setup
-from os import path
 
-here = path.abspath(path.dirname(__file__))
-# Get the long description from the relevant file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
-    long_description = f.read()
 
 setup(
     name='floodestimation',
     version='0.3.0',
     packages=['floodestimation'],
-    url='https://github.com/OpenHydrology/floodestimation',
-    license='GPLv3',
-    author='Neil Nutt, Florenz A. P. Hollebrandse',
-    author_email='f.a.p.hollebrandse@protonmail.ch',
-    description='Library for estimating flood flow rates',
-    long_description=long_description,
     install_requires=[
         'appdirs>=1.4,<1.5',
         'sqlalchemy>=0.9,<0.10',
@@ -27,18 +16,4 @@ setup(
         'floodestimation': ['fehdata.json',
                             'config.ini'],
     },
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Other Environment",
-        "Intended Audience :: Education",
-        "Intended Audience :: End Users/Desktop",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Operating System :: OS Independent",
-        "Topic :: Scientific/Engineering",
-        "Topic :: Scientific/Engineering :: Physics",
-        "Topic :: Scientific/Engineering :: Mathematics",
-        "Topic :: Software Development :: Libraries :: Python Modules"
-    ],
 )
