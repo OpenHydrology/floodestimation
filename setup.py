8a90c257298ab6a549123e1f2d196a08ce20b0ce
Florenz A. P. Hollebrandse <faph@users.noreply.github.com>
2015-02-16 17:50:03 +0000

    use VERSION file

--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
 from setuptools import setup
+from os import path
+import version
+
+# Update version from GIT tags if possible and create version file
+version.update()
+
+here = path.abspath(path.dirname(__file__))
+version = open(path.join(here, 'VERSION')).read().split('-')[0]
 
 
 setup(
