1c32cfe929e92ef60d374246193d93e05ee117db
Florenz A. P. Hollebrandse <faph@users.noreply.github.com>
2015-02-23 21:28:41 +0000

    remove requirements from setup

--- setup.py
+++ setup.py
@@ -13,13 +13,6 @@ setup(
     name='floodestimation',
     version=version,
     packages=['floodestimation'],
-    install_requires=[
-        'appdirs>=1.4,<1.5',
-        'sqlalchemy>=0.9,<0.10',
-        'numpy>=1.9,<1.10',
-        'scipy>=0.14',
-        'lmoments3>=1.0.2,<1.1'
-    ],
     package_data={
         'floodestimation': ['fehdata.json',
                             'config.ini'],
