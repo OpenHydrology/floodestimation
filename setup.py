e259ab6ec01b907a64e0fc9e0c8230ef271f9546
Florenz A. P. Hollebrandse <faph@users.noreply.github.com>
2015-09-07 18:32:56 +0100

    install versioneer

--- setup.py
+++ setup.py
@@ -1,21 +1,15 @@
 from setuptools import setup
-from os import path
-import version
-
-# Update version from GIT tags if possible and create version file
-version.update()
-
-here = path.abspath(path.dirname(__file__))
-version = open(path.join(here, 'VERSION')).read().split('-')[0]
+import versioneer
 
 
 setup(
     name='floodestimation',
-    version=version,
     packages=['floodestimation'],
     package_data={
         'floodestimation': ['fehdata.json',
                             'config.ini'],
     },
-    zip_safe=False
+    zip_safe=False,
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass()
 )
